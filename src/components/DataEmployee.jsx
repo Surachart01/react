979e5cc43e7122927478884d10df2ec4d4b6e73c
Surachart01 <titlemy36@gmail.com>
2024-02-12 16:21:49 +0700

    commit

--- src/components/DataEmployee.jsx
+++ src/components/DataEmployee.jsx
@@ -1,172 +1,202 @@
-import * as React from "react";
-import Paper from "@mui/material/Paper";
-import ForminputEmp from './ForminputEmp'
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import {
-  Box,
-  Button,
-  Container,
-  IconButton,
-  Typography,
-} from "@mui/material";
-import EditNoteIcon from "@mui/icons-material/EditNote";
-import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
-import { styled } from "@mui/material/styles";
-
-// format: (value) => value.toLocaleString("en-US")
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor:'#66CCCC',
-    color: theme.palette.common.white,
+import { DataGrid } from "@mui/x-data-grid";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Forminput from '../components/Forminput'
+const rows = [
+  {
+    id: 1,
+    empCode: "65309010001",
+    stuIdCard: "1739902009000",
+    title: "นาย",
+    name: "กฤษณชัย",
+    surname: "อุบลทิพย์",
+    username: "kritsanachai",
+    department: "SuperAdmin",
+    tel: "09876543321",
   },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+  {
+    id: 2,
+    empCode: "65309010002",
+    stuIdCard: "1739902009000",
+    title: "นาย",
+    name: "นันทวัฒน์",
+    surname: "มาศวิเศษ",
+    username: "nantawat",
+    department: "BranchAdmin",
+    tel: "0982079678",
   },
-}));
+  {
+    id: 3,
+    empCode: "65309010003",
+    stuIdCard: "1739902009000",
+    title: "นาย",
+    name: "กฤษณชัย",
+    surname: "อุบลทิพย์",
+    username: "kritsanachai",
+    department: "Branch Staff",
+    tel: "09876543321",
+  },
+  {
+    id: 4,
+    empCode: "65309010009",
+    stuIdCard: "1739902009000",
+    title: "นาย",
+    name: "กฤษณชัย",
+    surname: "อุบลทิพย์",
+    username: "Doctor",
+    department: "Branch Staff",
+    tel: "09876543321",
+  },
+  {
+    id: 5,
+    empCode: "65309010004",
+    stuIdCard: "1739902009000",
+    title: "นาย",
+    name: "กฤษณชัย",
+    surname: "อุบลทิพย์",
+    username: "kritsanachai",
+    department: "Doctor",
+    tel: "09876543321",
+  },
+  {
+    id: 6,
+    empCode: "65309010005",
+    stuIdCard: "1739902009000",
+    title: "นาย",
+    name: "กฤษณชัย",
+    surname: "อุบลทิพย์",
+    username: "kritsanachai",
+    department: "Nurse",
+    tel: "09876543321",
+  },
+  {
+    id: 7,
+    empCode: "65309010006",
+    stuIdCard: "1739902009000",
+    title: "นาง",
+    name: "กฤษณชัย",
+    surname: "อุบลทิพย์",
+    username: "kritsanachai",
+    department: "Nurse",
+    tel: "09876543321",
+  },
+];
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    "&:last-child td, &:last-child th": {
-      border: 0,
-    },
-  }));
+// เพิ่มคอลัมน์เลขลำดับ
+const rowsWithIndex = rows.map((row, index) => ({ ...row, index: index + 1 }));
 
 const columns = [
-  { id: "name", label: "ผู้ป่วย" },
-  { id: "age", label: "อายุ", align: "center" },
+  { field: "index", headerName: "#", width: 50 },
   {
-    id: "weight",
-    label: "น้ำหนัก",
-    align: "center",
-    format: (value) => value.toFixed(2),
+    field: "empCode",
+    headerName: "รหัสพนักงาน",
+    flex: 2,
+    minWidth: 100,
+    maxWidth: 150,
   },
   {
-    id: "height",
-    label: "ส่วนสูง",
-    align: "center",
-    format: (value) => value.toFixed(2),
+    field: "stuIdCard",
+    headerName: "หมายเลขบัตรประชาชน",
+    flex: 2,
+    minWidth: 150,
+    maxWidth: 200,
   },
   {
-    id: "position",
-    label: "ตำแหน่ง",
-    align: "center",
-    format: (value) => value.toFixed(2),
+    field: "title",
+    headerName: "คำนำหน้า",
+    flex: 1,
+    minWidth: 70,
+    maxWidth: 100,
   },
-  
+  { field: "name", headerName: "ชื่อ", flex: 2, minWidth: 100, maxWidth: 150 },
+  {
+    field: "surname",
+    headerName: "นามสกุล",
+    flex: 2,
+    minWidth: 100,
+    maxWidth: 150,
+  },
+  {
+    field: "username",
+    headerName: "ชื่อผู้ใช้งาน",
+    flex: 2,
+    minWidth: 120,
+    maxWidth: 200,
+  },
+  {
+    field: "department",
+    headerName: "สิทธิ",
+    flex: 2,
+    minWidth: 100,
+    maxWidth: 200,
+  },
+  {
+    field: "tel",
+    headerName: "เบอร์โทร",
+    flex: 2,
+    minWidth: 120,
+    maxWidth: 220,
+  },
+  {
+    field: "actions",
+    headerName: "Actions",
+    width: 200,
+    renderCell: (params) => (
+      <div>
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="outlined"
+            startIcon={<BorderColorIcon />}
+            onClick={() => handleEdit(params.row.id)}
+          >
+            แก้ไข
+          </Button>
+          <Button
+            variant="outlined"
+            color="error"
+            startIcon={<DeleteIcon />}
+            onClick={() => handleDelete(params.row.id)}
+          >
+            ลบ
+          </Button>
+        </Stack>
+      </div>
+    ),
+  },
 ];
 
-function createData(name, age, weight, height , position) {
-  return { name, age, weight, height ,position};
-}
+const handleEdit = (id) => {
+  // เขียนโค้ดสำหรับการแก้ไขข้อมูลด้วย ID ที่ได้รับ
+  console.log("Edit clicked for row with ID:", id);
+};
 
-const rows = [
-  createData("นันทวัฒน์ มาศวิเศษ", 20, 65, 174 , "SuperAdmin"),
-  createData("นายวิทยา", 28, 65, 180 , "BranchAdmin"),
-  createData("นางสาวสุภาพร", 25, 55, 165 ,"BranchAdmin"),
-  createData("นายณัฐพล", 35, 80, 175 , "Branch Staff"),
-  createData("นางอรทัย", 32, 60, 160 , "Branch Staff"),
-  createData("นายประพล", 40, 75, 178 , "Doctor"),
-  createData("นางสาวศุภรัตน์ ", 27, 52, 162 ,"Doctor"),
-  createData("นายสุรชัย", 38, 78, 172 , "Nurse"),
-  createData("นางจารุวรรณ", 29, 58, 168 ,"Nurse"),
-  createData("นายพีรศักดิ์", 33, 72, 176 , "Nurse"),
-  createData("นางสาวปราณี", 31, 65, 170 , "Nurse"),
-];
+const handleDelete = (id) => {
+  // เขียนโค้ดสำหรับการลบข้อมูลด้วย ID ที่ได้รับ
+  console.log("Delete clicked for row with ID:", id);
+};
 
-export default function StickyHeadTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
+const DataStudents = () => {
   return (
+
     <Box>
       <Container>
-        <Typography
-          sx={{
+        <Typography  sx={{
             textAlign: "center",
             marginBottom: 3,
             marginTop: 10,
             fontSize: 25,
-          }}
-        >
-          พนักงาน
+          }}>
+            ตารางพนักงาน
         </Typography>
-        <TableContainer sx={{ maxHeight: 440 }}>
-          <Table aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>ขื่อ-นามสกุล</StyledTableCell>
-                <StyledTableCell align="center">อายุ</StyledTableCell>
-                <StyledTableCell align="center">น้ำหนัก</StyledTableCell>
-                <StyledTableCell align="center">ส่วนสูง</StyledTableCell>
-                <StyledTableCell align="center">ตำเเหน่ง</StyledTableCell>
-                <StyledTableCell colSpan={2} align="center"></StyledTableCell>
-                
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <StyledTableRow hover role="checkbox" tabIndex={-1} key={row.age}>
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      })}
-                      <TableCell>
-                        <IconButton color="warning" sx={{ fontSize: 14 }}>
-                          <EditNoteIcon />
-                          เเก้ไข
-                        </IconButton>
-                      </TableCell>
-                      <TableCell>
-                        <IconButton color="error" sx={{ fontSize: 14 }}>
-                          <DeleteForeverOutlinedIcon />
-                          ลบ
-                        </IconButton>
-                      </TableCell>
-                    </StyledTableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-          <ForminputEmp/>
+        <DataGrid rows={rowsWithIndex} columns={columns} />
+        <Forminput/>
       </Container>
     </Box>
+
+
+
   );
-}
+};
+
+export default DataStudents;
